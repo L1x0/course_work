fbf888c968338dbe6c6190583a6ba65b4ea5127a
Астахов Артём <artemastahov27@gmail.com>
2024-04-08 23:21:10 +0300

    Создал структуру

---
+++ CourseWork/CourseWork/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  CourseWork
+//
+//  Created by Артём Астахов on 8.04.24.
+//
+
+#include <iostream>
+using namespace std;
+
+struct Buyer {
+    string number;
+    string name;
+    string address;
+    string accountingDate;
+};
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
