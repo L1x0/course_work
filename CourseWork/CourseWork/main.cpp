df6213db1381046194c751a72ca942133a9321be
Астахов Артём <artemastahov27@gmail.com>
2024-04-09 22:46:10 +0300

    Реализовал ввод-вывод

--- CourseWork/CourseWork/main.cpp
+++ CourseWork/CourseWork/main.cpp
@@ -6,6 +6,9 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
 using namespace std;
 
 struct Buyer {
@@ -15,7 +18,69 @@ struct Buyer {
     string accountingDate;
 };
 
+vector<Buyer> getData(vector<Buyer> buyers) {
+    ifstream file("input.txt");
+    
+    if (file.is_open()) {
+        string temp;
+
+        while (!file.eof()) {
+            Buyer buyer;
+            
+            getline(file, temp);
+            temp.replace(0, 8, "");
+            buyer.number = temp;
+            
+            getline(file, temp);
+            temp.replace(0, 6, "");
+            buyer.name = temp;
+            
+            getline(file, temp);
+            temp.replace(0, 9, "");
+            buyer.address = temp;
+            
+            getline(file, temp);
+            temp.replace(0, 16, "");
+            buyer.accountingDate = temp;
+            
+            buyers.push_back(buyer);
+            
+            getline(file, temp);
+        }
+        
+    } else {
+        cout << "Файл для чтения не открыт" << endl;
+    }
+    
+    file.close();
+    
+    return buyers;
+}
+
+void putData(vector<Buyer> buyers) {
+    ofstream file("output.txt");
+    if (file.is_open()) {
+        for (int i = 0; i < buyers.size(); i++) {
+            Buyer buyer = buyers.at(i);
+            
+            file << "number: " << buyer.number << endl;
+            file << "name: " << buyer.name << endl;
+            file << "address: " << buyer.address << endl;
+            file << "accounting date: " << buyer.accountingDate << endl;
+            file << "\n";
+            
+        }
+    } else {
+        cout << "Файл для записи не открыт" << endl;
+    }
+    
+    file.close();
+}
+
 int main(int argc, const char * argv[]) {
+    vector<Buyer> buyers;
+    buyers = getData(buyers);
+    putData(buyers);
     
     return 0;
 }
