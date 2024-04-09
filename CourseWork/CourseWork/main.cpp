f73ea6220aa2214066220dd1fd265a9e8d7f9753
Астахов Артём <artemastahov27@gmail.com>
2024-04-09 23:49:50 +0300

    Добавил Bubble Sort

--- CourseWork/CourseWork/main.cpp
+++ CourseWork/CourseWork/main.cpp
@@ -59,6 +59,7 @@ vector<Buyer> getData(vector<Buyer> buyers) {
 
 void putData(vector<Buyer> buyers) {
     ofstream file("output.txt");
+    
     if (file.is_open()) {
         for (int i = 0; i < buyers.size(); i++) {
             Buyer buyer = buyers.at(i);
@@ -68,7 +69,6 @@ void putData(vector<Buyer> buyers) {
             file << "address: " << buyer.address << endl;
             file << "accounting date: " << buyer.accountingDate << endl;
             file << "\n";
-            
         }
     } else {
         cout << "Файл для записи не открыт" << endl;
@@ -77,9 +77,58 @@ void putData(vector<Buyer> buyers) {
     file.close();
 }
 
+vector<Buyer> bubbleSort_Date(vector<Buyer> buyers) {
+    bool protect = true;
+    
+    while(protect) {
+        protect = false;
+        
+        for (int i = 1; i < buyers.size(); i++) {
+            int yearsOfFisrt, mounthOfFirst, daysOfFirst, yearsOfSecond, mounthOfSecond, daysOfSecond;
+            
+            daysOfFirst = stoi(buyers[i - 1].accountingDate.substr(0,2));
+            mounthOfFirst = stoi(buyers[i - 1].accountingDate.substr(3,2));
+            yearsOfFisrt = stoi(buyers[i - 1].accountingDate.substr(6,buyers[i].accountingDate.size() - 6));
+            
+            daysOfSecond = stoi(buyers[i].accountingDate.substr(0,2));
+            mounthOfSecond = stoi(buyers[i].accountingDate.substr(3,2));
+            yearsOfSecond = stoi(buyers[i].accountingDate.substr(6,buyers[i].accountingDate.size() - 6));
+            
+            if (yearsOfFisrt > yearsOfSecond) {
+                swap(buyers[i - 1], buyers[i]);
+                protect = true;
+                continue;
+            }
+            
+            if (yearsOfFisrt == yearsOfSecond) {
+                
+                if (mounthOfFirst > mounthOfSecond) {
+                    swap(buyers[i - 1], buyers[i]);
+                    protect = true;
+                    continue;
+                }
+                
+                if (mounthOfFirst == mounthOfSecond) {
+                    
+                    if (daysOfFirst > daysOfSecond) {
+                        swap(buyers[i - 1], buyers[i]);
+                        protect = true;
+                        continue;
+                    }
+                }
+                    
+            }
+        }
+        
+    }
+    
+    return buyers;
+}
+
 int main(int argc, const char * argv[]) {
     vector<Buyer> buyers;
     buyers = getData(buyers);
+    buyers = bubbleSort_Date(buyers);
     putData(buyers);
     
     return 0;
